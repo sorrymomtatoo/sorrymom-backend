ef9ca6fe9d5d45c8181f5f5046ba7b8e7d768dae
sorrymomtatoo <2026337847@aluno.poa.ifrs.edu.br>
2026-05-09 17:45:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,47 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from datetime import datetime
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+orcamentos = []
+
+class Orcamento(BaseModel):
+    tipo: str
+    nome: str
+    idade: str
+    telefone: str
+    detalhes: dict
 
 @app.get("/")
 def home():
     return {"message": "Sorry Mom Backend Online"}
+
+@app.post("/orcamentos")
+def criar_orcamento(orcamento: Orcamento):
+    novo = {
+        "id": len(orcamentos) + 1,
+        "data": datetime.now().strftime("%d/%m/%Y %H:%M"),
+        **orcamento.dict()
+    }
+
+    orcamentos.append(novo)
+
+    return {
+        "status": "sucesso",
+        "message": "Orçamento recebido com sucesso",
+        "orcamento": novo
+    }
+
+@app.get("/orcamentos")
+def listar_orcamentos():
+    return orcamentos
